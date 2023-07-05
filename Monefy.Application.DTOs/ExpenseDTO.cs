1249faa44b316152d37ea1969cc7cdf56ee20ef5
Lev4ni <levani.makharashvili@gmail.com>
2023-07-05 09:02:03 +0200

    DTO updated

--- Monefy.Application.DTOs/ExpenseDTO.cs
+++ Monefy.Application.DTOs/ExpenseDTO.cs
@@ -1,10 +1,13 @@
 ﻿
+using Monefy.Infraestructure.DataModels;
+
 namespace Monefy.Application.DTOs
 {
     public class ExpenseDTO
     {
         public int Id { get; set; }
-        public int CategoryId { get; set; }
+       // public int CategoryId { get; set; }
+        public Category Category { get;set; }
         public decimal Amount { get; set; }
         public string? Description { get; set; }
         public DateTime DateCreation { get; set; }
