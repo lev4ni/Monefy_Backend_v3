9662677fe30f3c7a84a4c13c12305b31b55b93e0
Lev4ni <levani.makharashvili@gmail.com>
2023-07-05 09:13:06 +0200

    added get all wallets swagger

--- Monefy.Application.DTOs/ExpenseDTO.cs
+++ Monefy.Application.DTOs/ExpenseDTO.cs
@@ -7,7 +7,7 @@ namespace Monefy.Application.DTOs
     {
         public int Id { get; set; }
        // public int CategoryId { get; set; }
-        public Category Category { get;set; }
+        public Category? Category { get;set; }
         public decimal Amount { get; set; }
         public string? Description { get; set; }
         public DateTime DateCreation { get; set; }
---
+++ Monefy.DistribuitedWebService/Controllers/WalletController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monefy.Application.Contracts;
+using Monefy.Application.Implementation;
+
+namespace Monefy.DistribuitedWebService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : Controller
+    {
+        private readonly IWalletAppService _walletAppService;
+        public WalletController(IWalletAppService walletAppService)
+        {
+            _walletAppService = walletAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWallets()
+        {
+            var wallet = await _walletAppService.GetAllWalletsAsync();
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+            return Ok(wallet);
+        }
+    }
+}
