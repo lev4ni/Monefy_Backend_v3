d017d3dcb76c72c59e1749ff6db2d5cc1d6a2168
Lev4ni <levani.makharashvili@gmail.com>
2023-07-13 17:33:57 +0200

    Validators DTO Done

---
+++ Monefy.Application/Services/IncomeDTOValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Monefy.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monefy.Application.Services
+{
+    public class IncomeDTOValidator : AbstractValidator<IncomeDTO>
+    {
+        public IncomeDTOValidator() 
+        {
+            RuleFor(x => x.Description)
+             .NotEmpty().WithMessage("Description can't be empty");
+            RuleFor(x => x.Amount)
+                .NotEmpty().WithMessage("Please, add Amount!");
+        }
+
+    }
+}
---
+++ Monefy.Application/Services/WalletDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Monefy.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monefy.Application.Services
+{
+    public class WalletDTOValidator : AbstractValidator<WalletDTO>
+    {
+        public WalletDTOValidator()
+        {
+            RuleFor(x => x.Name)
+             .NotEmpty().WithMessage("Name for this wallet can't be empty");
+        }
+    }
+}
